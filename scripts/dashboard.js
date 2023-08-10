f4835a445c762b1a176eb0f7660ec2f48d2093e9
Brian Mendez <brianmendez2308@gmail.com>
2023-08-10 15:19:51 -0700

    dashboard html and css design template

---
+++ scripts/dashboard.js
@@ -0,0 +1,14 @@
+function createInvoice() {
+    // Functionality to create an invoice
+    alert('Creating Invoice');
+}
+
+function createEstimate() {
+    // Functionality to create an estimate
+    alert('Creating Estimate');
+}
+
+function addClient() {
+    // Functionality to add a client
+    alert('Adding Client');
+}
