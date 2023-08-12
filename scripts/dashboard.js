3c713eb6f512c15765a0616ff3c6df15878ff0f0
Brian Mendez <brianmendez2308@gmail.com>
2023-08-12 09:58:33 -0700

    implementing initial js

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -1,14 +1,35 @@
-function createInvoice() {
-    // Functionality to create an invoice
-    alert('Creating Invoice');
+function init() {
+    let element = document.getElementById("add-client");
+    element.addEventListener("click", function () {
+      addClient();
+    });
+
 }
 
-function createEstimate() {
-    // Functionality to create an estimate
-    alert('Creating Estimate');
+function addClient(){
+    let date = document.getElementById('client-date').value;
+    let name = document.getElementById('client-name').value;
+    let description = document.getElementById('client-description').value;
+    let amount = document.getElementById('client-amount').value;
+    let status = document.getElementById('client-status').value;
+
+    let tbody = document.getElementById('client-table-body');
+
+    let row = tbody.insertRow();
+
+    row.insertCell(0).textContent = date;
+    row.insertCell(1).textContent = name;
+    row.insertCell(2).textContent = description;
+    row.insertCell(3).textContent = amount;
+    row.insertCell(4).textContent = status;
+    
+    // You can also add actions like delete or edit in the last cell if required.
+    let deleteBtn = document.createElement('button');
+    deleteBtn.textContent = 'Delete';
+    deleteBtn.onclick = function() {
+        tbody.removeChild(row);
+    };
+    row.insertCell(5).appendChild(deleteBtn);
 }
 
-function addClient() {
-    // Functionality to add a client
-    alert('Adding Client');
-}
+window.addEventListener("DOMContentLoaded", init);
