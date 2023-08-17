83ce888e19264473ea050d8eb3446e8ee704c7ce
Brian Mendez <brianmendez2308@gmail.com>
2023-08-16 23:50:14 -0700

    pdf-lib API complete with Invoice
    Date, Description, and Name have been included from the local storage and drawn onto the Invoice.pdf template created.

--- scripts/invoice.js
+++ scripts/invoice.js
@@ -1,162 +1,107 @@
-function init() {
-    let el = document.getElementById("add-invoice");
-    el.addEventListener("click", function() {
-        selectClientAndCreateInvoice();
-    });
+function getClient(clientName) {
+  let allClients = JSON.parse(localStorage.getItem("clients")) || [];
+  return allClients.find((client) => client.name === clientName) || null;
 }
 
-function selectClientAndCreateInvoice() {
-    // Fetch all clients from localStorage
-    let clients = JSON.parse(localStorage.getItem('clients'));
-    
-    if (!clients || !clients.length) {
-        alert("No clients available!");
-        return;
-    }
+async function generateInvoice(client) {
+  const { PDFDocument, rgb, StandardFonts } = PDFLib;
+  const url = "/assets/Invoice.pdf";
+  const existingPdfBytes = await fetch(url).then((res) => res.arrayBuffer());
 
-    // Present a dropdown or modal to select a client
-    // For simplicity, using a prompt here:
-    let clientNames = clients.map((c, index) => `${index + 1}. ${c.name}`).join('\n');
-    let clientIndex = parseInt(prompt(`Select a client by entering its number:\n${clientNames}`)) - 1;
-    
-    if (isNaN(clientIndex) || clientIndex < 0 || clientIndex >= clients.length) {
-        alert("Invalid selection.");
-        return;
-    }
+  let pdfDoc = await PDFDocument.load(existingPdfBytes);
+  let customFont = await pdfDoc.embedFont(StandardFonts.CourierBold);
 
-    let selectedClient = clients[clientIndex];
-    generateInvoiceForClient(selectedClient);
+  let pages = pdfDoc.getPages();
+  let firstPage = pages[0];
+
+  let fontSize = 12;
+  let spacing = 2;
+  let xPositionInfo = 179.28;
+  let yPositionDate = 589.56;
+  let yPositionName = 563.56;
+  let yPositionAddress = 536.32;
+
+  let xPositionPrice = 468;
+  let xPositionRate = 403.2;
+
+  let yPositionServicePrice = 439.2;
+  let yPositionTrimPrice = 405.36;
+  let yPositionCleanUpPrice = 372.24;
+  let yPositionIrrigationPrice = 339.12;
+  let yPositionPlantsPrice = 306;
+  let yPositionOtherPrice = 272.88;
+
+  // Date
+  let date = new Date();
+  let lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0);
+  let monthNames = ["JANUARY", "FEBRUARY", "MARCH", "APRIL", "MAY", "JUNE", "JULY", "AUGUST", "SEPTEMBER", "OCTOBER", "NOVEMBER", "DECEMBER",];
+  let formattedDate = `${monthNames[lastDay.getMonth()]} ${lastDay.getDate()}, ${lastDay.getFullYear()}`;
+  let xPosDate = xPositionInfo;
+  for(let i = 0; i < formattedDate.length; i++){
+    firstPage.drawText(formattedDate[i], {
+        x: xPosDate,
+        y: yPositionDate,
+        size: fontSize,
+        font: customFont,
+        color: rgb(0, 0, 0),
+      });
+    xPosDate += customFont.widthOfTextAtSize(formattedDate[i], 12) + spacing;
+  }
+
+  // Client's name
+  let name = client.name.toUpperCase();
+  let xPosName = xPositionInfo; 
+  for(let i = 0; i < name.length; i++){
+    firstPage.drawText(name[i], {
+        x: xPosName,
+        y: yPositionName,
+        size: fontSize,
+        font: customFont,
+        color: rgb(0, 0, 0),
+      });
+    xPosName += customFont.widthOfTextAtSize(name[i], 12) + spacing;
+  }
+
+  let addr = client.description.toUpperCase();
+  let xPosAddr = xPositionInfo;
+  for (let i = 0; i < addr.length; i++){
+    firstPage.drawText(addr[i], {
+        x: xPosAddr,
+        y: yPositionAddress,
+        size: fontSize,
+        font: customFont,
+        color: rgb(0, 0, 0),
+      });
+    xPosAddr += customFont.widthOfTextAtSize(addr[i], 12) + spacing;
+  }
+
+  let pdfBytes = await pdfDoc.save();
+
+  download(pdfBytes, `${client.name}-Invoice.pdf`, "application/pdf");
 }
 
-function generateInvoiceForClient(clientData) {
-    // Modify the props object with the clientData
-    props.contact.name = clientData.name;
-    props.contact.address = clientData.address;
-    // ... do this for all the fields you need
+function populateClientsDropdown() {
+  let clientSelect = document.getElementById("clientSelect");
+  let allClients = JSON.parse(localStorage.getItem("clients")) || [];
 
-    // Use the updated props to generate the invoice
-    const pdfObject = jsPDFInvoiceTemplate.default(props);
-
-    // Here, you can save the PDF or email it or whatever your next steps are
+  allClients.forEach((client) => {
+    let option = document.createElement("option");
+    option.value = client.name;
+    option.text = client.name;
+    clientSelect.appendChild(option);
+  });
 }
 
-window.addEventListener("DOMContentLoaded", function() {
-    init();
+function generateSelectedClientInvoice() {
+  let clientName = document.getElementById("clientSelect").value;
+  let client = getClient(clientName);
+  if (client) {
+    generateInvoice(client);
+  }
+}
+
+window.onload = populateClientsDropdown;
+window.addEventListener("DOMContentLoaded", function () {
+  let el = document.getElementById("add-invoice");
+  el.addEventListener("click", generateSelectedClientInvoice);
 });
-
-var props = {
-    outputType: OutputType.Save,
-    returnJsPDFDocObject: true,
-    fileName: "Invoice 2021",
-    orientationLandscape: false,
-    compress: true,
-    logo: {
-        src: "https://raw.githubusercontent.com/edisonneza/jspdf-invoice-template/demo/images/logo.png",
-        type: 'PNG', //optional, when src= data:uri (nodejs case)
-        width: 53.33, //aspect ratio = width/height
-        height: 26.66,
-        margin: {
-            top: 0, //negative or positive num, from the current position
-            left: 0 //negative or positive num, from the current position
-        }
-    },
-    stamp: {
-        inAllPages: true, //by default = false, just in the last page
-        src: "https://raw.githubusercontent.com/edisonneza/jspdf-invoice-template/demo/images/qr_code.jpg",
-        type: 'JPG', //optional, when src= data:uri (nodejs case)
-        width: 20, //aspect ratio = width/height
-        height: 20,
-        margin: {
-            top: 0, //negative or positive num, from the current position
-            left: 0 //negative or positive num, from the current position
-        }
-    },
-    business: {
-        name: "Business Name",
-        address: "Albania, Tirane ish-Dogana, Durres 2001",
-        phone: "(+355) 069 11 11 111",
-        email: "email@example.com",
-        email_1: "info@example.al",
-        website: "www.example.al",
-    },
-    contact: {
-        label: "Invoice issued for:",
-        name: "Client Name",
-        address: "Albania, Tirane, Astir",
-        phone: "(+355) 069 22 22 222",
-        email: "client@website.al",
-        otherInfo: "www.website.al",
-    },
-    invoice: {
-        label: "Invoice #: ",
-        num: 19,
-        invDate: "Payment Date: 01/01/2021 18:12",
-        invGenDate: "Invoice Date: 02/02/2021 10:17",
-        headerBorder: false,
-        tableBodyBorder: false,
-        header: [
-          {
-            title: "#", 
-            style: { 
-              width: 10 
-            } 
-          }, 
-          { 
-            title: "Title",
-            style: {
-              width: 30
-            } 
-          }, 
-          { 
-            title: "Description",
-            style: {
-              width: 80
-            } 
-          }, 
-          { title: "Price"},
-          { title: "Quantity"},
-          { title: "Unit"},
-          { title: "Total"}
-        ],
-        table: Array.from(Array(10), (item, index)=>([
-            index + 1,
-            "There are many variations ",
-            "Lorem Ipsum is simply dummy text dummy text ",
-            200.5,
-            4.5,
-            "m2",
-            400.5
-        ])),
-        additionalRows: [{
-            col1: 'Total:',
-            col2: '145,250.50',
-            col3: 'ALL',
-            style: {
-                fontSize: 14 //optional, default 12
-            }
-        },
-        {
-            col1: 'VAT:',
-            col2: '20',
-            col3: '%',
-            style: {
-                fontSize: 10 //optional, default 12
-            }
-        },
-        {
-            col1: 'SubTotal:',
-            col2: '116,199.90',
-            col3: 'ALL',
-            style: {
-                fontSize: 10 //optional, default 12
-            }
-        }],
-        invDescLabel: "Invoice Note",
-        invDesc: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary.",
-    },
-    footer: {
-        text: "The invoice is created on a computer and is valid without the signature and stamp.",
-    },
-    pageEnable: true,
-    pageLabel: "Page ",
-};
