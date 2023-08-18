100671f8fa36bbacc67dab76c124f96b35b884cb
Brian Mendez <brianmendez2308@gmail.com>
2023-08-18 15:33:26 -0700

    Popup invoice form and corrected spacing when generating invoice

--- scripts/invoice.js
+++ scripts/invoice.js
@@ -3,7 +3,7 @@ function getClient(clientName) {
   return allClients.find((client) => client.name === clientName) || null;
 }
 
-async function generateInvoice(client) {
+async function generateInvoice(client, additionalServices =[] ) {
   const { PDFDocument, rgb, StandardFonts } = PDFLib;
   const url = "/assets/Invoice.pdf";
   const existingPdfBytes = await fetch(url).then((res) => res.arrayBuffer());
@@ -21,15 +21,15 @@ async function generateInvoice(client) {
   let yPositionName = 563.56;
   let yPositionAddress = 536.32;
 
-  let xPositionPrice = 468;
+  let xPositionPrice = 490;
   let xPositionRate = 403.2;
 
-  let yPositionServicePrice = 439.2;
-  let yPositionTrimPrice = 405.36;
-  let yPositionCleanUpPrice = 372.24;
-  let yPositionIrrigationPrice = 339.12;
-  let yPositionPlantsPrice = 306;
-  let yPositionOtherPrice = 272.88;
+  let yPositionServicePrice = 402.5;
+  let yPositionTrimPrice = 368.66;
+  let yPositionCleanUpPrice = 335.96;
+  let yPositionIrrigationPrice = 302.26;
+  let yPositionPlantsPrice = 268.56;
+  let yPositionOtherPrice = 234.86;
 
   // Date
   let date = new Date();
@@ -75,6 +75,54 @@ async function generateInvoice(client) {
     xPosAddr += customFont.widthOfTextAtSize(addr[i], 12) + spacing;
   }
 
+  firstPage.drawText(`$ ${client.amount}.00`, {
+      x: xPositionPrice, 
+      y: yPositionServicePrice, 
+      size: fontSize, 
+      font: customFont,
+      color: rgb(0,0,0),
+  });
+
+//   firstPage.drawText(`$ ${client.amount}.00`, {
+//     x: xPositionPrice, 
+//     y: yPositionTrimPrice, 
+//     size: fontSize, 
+//     font: customFont,
+//     color: rgb(0,0,0),
+// });
+
+// firstPage.drawText(`$ ${client.amount}.00`, {
+//   x: xPositionPrice, 
+//   y: yPositionCleanUpPrice, 
+//   size: fontSize, 
+//   font: customFont,
+//   color: rgb(0,0,0),
+// });
+
+// firstPage.drawText(`$ ${client.amount}.00`, {
+//   x: xPositionPrice, 
+//   y: yPositionIrrigationPrice, 
+//   size: fontSize, 
+//   font: customFont,
+//   color: rgb(0,0,0),
+// });
+
+// firstPage.drawText(`$ ${client.amount}.00`, {
+//   x: xPositionPrice, 
+//   y: yPositionPlantsPrice, 
+//   size: fontSize, 
+//   font: customFont,
+//   color: rgb(0,0,0),
+// });
+
+// firstPage.drawText(`$ ${client.amount}.00`, {
+//   x: xPositionPrice, 
+//   y: yPositionOtherPrice, 
+//   size: fontSize, 
+//   font: customFont,
+//   color: rgb(0,0,0),
+// });
+
   let pdfBytes = await pdfDoc.save();
 
   download(pdfBytes, `${client.name}-Invoice.pdf`, "application/pdf");
@@ -93,15 +141,62 @@ function populateClientsDropdown() {
 }
 
 function generateSelectedClientInvoice() {
-  let clientName = document.getElementById("clientSelect").value;
   let client = getClient(clientName);
   if (client) {
     generateInvoice(client);
   }
 }
 
-window.onload = populateClientsDropdown;
+function showInvoiceForm() {
+  document.getElementById("invoice-popup").style.display = "block";
+}
+
+function closeInvoiceForm() {
+  document.getElementById("invoice-popup").style.display = "none";
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  let trimValue = parseFloat(document.getElementById("trim").value) || 0;
+  let cleanUpValue = parseFloat(document.getElementById("cleanUp").value) || 0;
+
+  let clientName = document.getElementById("clientSelect").value;
+  let client = getClient(clientName);
+
+  if (client) {
+      generateInvoice(client, {
+          trim: trimValue,
+          cleanUp: cleanUpValue
+      });
+  }
+
+  // Close the form after generating the invoice
+  closeInvoiceForm();
+}
+
+
 window.addEventListener("DOMContentLoaded", function () {
-  let el = document.getElementById("add-invoice");
-  el.addEventListener("click", generateSelectedClientInvoice);
+  // When the document is loaded, attach event listeners
+
+  // Attach event listener to show the form
+  let addInvoiceButton = document.getElementById("add-invoice");
+  addInvoiceButton.addEventListener("click", showInvoiceForm);
+
+  // Attach event listener to close the form
+  let closeButton = document.getElementById("invoice-close-button");
+  closeButton.addEventListener("click", closeInvoiceForm);
+
+  // Attach event listener to form submission
+  let invoiceForm = document.querySelector("#invoice-popup form");
+  invoiceForm.addEventListener("submit", handleSubmit);
+
+  // Populate clients dropdown
+  populateClientsDropdown();
 });
+
+
+
+
+
+
