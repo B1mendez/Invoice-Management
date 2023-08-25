74ffa42d0369527075be942432f07f254b5729e0
Brian Mendez <brianmendez2308@gmail.com>
2023-08-24 17:52:46 -0700

    additional services added to invoice template. Logo added

--- README.md
+++ README.md
@@ -2,7 +2,6 @@
 Invoice Management software for Daniel Mendez Landscaping 
 
 ## Todo
-- Figure out a way to include the service, rate, irrigation system etc without having to save every element instaneously. 
 - Fix CSS dropdown menu layout
 - Transition to electron js API
 - store client information in json package.
--- scripts/invoice.js
+++ scripts/invoice.js
@@ -3,25 +3,29 @@ function getClient(clientName) {
   return allClients.find((client) => client.name === clientName) || null;
 }
 
-async function generateInvoice(client, additionalServices =[] ) {
+async function generateInvoice(client, additionalServices = []) {
   const { PDFDocument, rgb, StandardFonts } = PDFLib;
   const url = "/assets/Invoice.pdf";
   const existingPdfBytes = await fetch(url).then((res) => res.arrayBuffer());
 
   let pdfDoc = await PDFDocument.load(existingPdfBytes);
   let customFont = await pdfDoc.embedFont(StandardFonts.CourierBold);
+  let priceFont = await pdfDoc.embedFont(StandardFonts.Courier);
 
   let pages = pdfDoc.getPages();
   let firstPage = pages[0];
 
   let fontSize = 12;
   let spacing = 2;
+
+  //General Information positioning
   let xPositionInfo = 179.28;
   let yPositionDate = 589.56;
   let yPositionName = 563.56;
   let yPositionAddress = 536.32;
 
-  let xPositionPrice = 490;
+  //additionalServices positioning
+  let xPositionPrice = 480;
   let xPositionRate = 403.2;
 
   let yPositionServicePrice = 402.5;
@@ -30,99 +34,108 @@ async function generateInvoice(client, additionalServices =[] ) {
   let yPositionIrrigationPrice = 302.26;
   let yPositionPlantsPrice = 268.56;
   let yPositionOtherPrice = 234.86;
+  let yPositionTotalPrice = 200.86;
 
-  // Date
+  let totalPrice = parseFloat(client.amount);
+  
+  // Date template 
   let date = new Date();
   let lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0);
-  let monthNames = ["JANUARY", "FEBRUARY", "MARCH", "APRIL", "MAY", "JUNE", "JULY", "AUGUST", "SEPTEMBER", "OCTOBER", "NOVEMBER", "DECEMBER",];
-  let formattedDate = `${monthNames[lastDay.getMonth()]} ${lastDay.getDate()}, ${lastDay.getFullYear()}`;
+  let monthNames = [
+    "JANUARY",
+    "FEBRUARY",
+    "MARCH",
+    "APRIL",
+    "MAY",
+    "JUNE",
+    "JULY",
+    "AUGUST",
+    "SEPTEMBER",
+    "OCTOBER",
+    "NOVEMBER",
+    "DECEMBER",
+  ];
+  let formattedDate = `${
+    monthNames[lastDay.getMonth()]
+  } ${lastDay.getDate()}, ${lastDay.getFullYear()}`;
   let xPosDate = xPositionInfo;
-  for(let i = 0; i < formattedDate.length; i++){
+  for (let i = 0; i < formattedDate.length; i++) {
     firstPage.drawText(formattedDate[i], {
-        x: xPosDate,
-        y: yPositionDate,
-        size: fontSize,
-        font: customFont,
-        color: rgb(0, 0, 0),
-      });
+      x: xPosDate,
+      y: yPositionDate,
+      size: fontSize,
+      font: customFont,
+      color: rgb(0, 0, 0),
+    });
     xPosDate += customFont.widthOfTextAtSize(formattedDate[i], 12) + spacing;
   }
 
   // Client's name
   let name = client.name.toUpperCase();
-  let xPosName = xPositionInfo; 
-  for(let i = 0; i < name.length; i++){
+  let xPosName = xPositionInfo;
+  for (let i = 0; i < name.length; i++) {
     firstPage.drawText(name[i], {
-        x: xPosName,
-        y: yPositionName,
-        size: fontSize,
-        font: customFont,
-        color: rgb(0, 0, 0),
-      });
+      x: xPosName,
+      y: yPositionName,
+      size: fontSize,
+      font: customFont,
+      color: rgb(0, 0, 0),
+    });
     xPosName += customFont.widthOfTextAtSize(name[i], 12) + spacing;
   }
 
   let addr = client.description.toUpperCase();
   let xPosAddr = xPositionInfo;
-  for (let i = 0; i < addr.length; i++){
+  for (let i = 0; i < addr.length; i++) {
     firstPage.drawText(addr[i], {
-        x: xPosAddr,
-        y: yPositionAddress,
-        size: fontSize,
-        font: customFont,
-        color: rgb(0, 0, 0),
-      });
+      x: xPosAddr,
+      y: yPositionAddress,
+      size: fontSize,
+      font: customFont,
+      color: rgb(0, 0, 0),
+    });
     xPosAddr += customFont.widthOfTextAtSize(addr[i], 12) + spacing;
   }
 
   firstPage.drawText(`$ ${client.amount}.00`, {
-      x: xPositionPrice, 
-      y: yPositionServicePrice, 
-      size: fontSize, 
-      font: customFont,
-      color: rgb(0,0,0),
+    x: xPositionPrice,
+    y: yPositionServicePrice,
+    size: fontSize,
+    font: priceFont,
+    color: rgb(0, 0, 0),
   });
 
-//   firstPage.drawText(`$ ${client.amount}.00`, {
-//     x: xPositionPrice, 
-//     y: yPositionTrimPrice, 
-//     size: fontSize, 
-//     font: customFont,
-//     color: rgb(0,0,0),
-// });
+  let serviceDetails = [
+    { service: 'trim', yPos: yPositionTrimPrice },
+    { service: 'cleanUp', yPos: yPositionCleanUpPrice },
+    { service: 'irrigation', yPos: yPositionIrrigationPrice },
+    { service: 'plants', yPos: yPositionPlantsPrice },
+    { service: 'other', yPos: yPositionOtherPrice },
+  ];
+  
+  for (let detail of serviceDetails) {
+    let serviceValue = additionalServices[detail.service];
+    if (serviceValue) { // only draw if there's a value
+      firstPage.drawText(`$ ${serviceValue}.00`, {
+        x: xPositionPrice,
+        y: detail.yPos,
+        size: fontSize,
+        font: priceFont,
+        color: rgb(0, 0, 0),
+      });
+      totalPrice = totalPrice + parseFloat(serviceValue);
+    }
+  }
 
-// firstPage.drawText(`$ ${client.amount}.00`, {
-//   x: xPositionPrice, 
-//   y: yPositionCleanUpPrice, 
-//   size: fontSize, 
-//   font: customFont,
-//   color: rgb(0,0,0),
-// });
-
-// firstPage.drawText(`$ ${client.amount}.00`, {
-//   x: xPositionPrice, 
-//   y: yPositionIrrigationPrice, 
-//   size: fontSize, 
-//   font: customFont,
-//   color: rgb(0,0,0),
-// });
-
-// firstPage.drawText(`$ ${client.amount}.00`, {
-//   x: xPositionPrice, 
-//   y: yPositionPlantsPrice, 
-//   size: fontSize, 
-//   font: customFont,
-//   color: rgb(0,0,0),
-// });
-
-// firstPage.drawText(`$ ${client.amount}.00`, {
-//   x: xPositionPrice, 
-//   y: yPositionOtherPrice, 
-//   size: fontSize, 
-//   font: customFont,
-//   color: rgb(0,0,0),
-// });
-
+  firstPage.drawText(`$ ${totalPrice.toFixed(2)}`, {
+    x: xPositionPrice,
+    y: yPositionTotalPrice,
+    size: fontSize+2,
+    font: customFont,
+    color: rgb(0, 0, 0),
+  });
+  
+  
   let pdfBytes = await pdfDoc.save();
 
   download(pdfBytes, `${client.name}-Invoice.pdf`, "application/pdf");
@@ -160,25 +173,29 @@ function handleSubmit(event) {
 
   let trimValue = parseFloat(document.getElementById("trim").value) || 0;
   let cleanUpValue = parseFloat(document.getElementById("cleanUp").value) || 0;
+  let irrigationValue = parseFloat(document.getElementById("irrigation").value) || 0;
+  let plantValue = parseFloat(document.getElementById("plant").value) || 0;
+  let otherValue = parseFloat(document.getElementById("other").value) || 0;
 
   let clientName = document.getElementById("clientSelect").value;
   let client = getClient(clientName);
 
   if (client) {
-      generateInvoice(client, {
-          trim: trimValue,
-          cleanUp: cleanUpValue
-      });
+    generateInvoice(client, {
+      trim: trimValue,
+      cleanUp: cleanUpValue,
+      irrigation: irrigationValue,
+      plants: plantValue,
+      other: otherValue,
+    });
   }
 
   // Close the form after generating the invoice
+  document.querySelector('#invoice-popup form').reset();
   closeInvoiceForm();
 }
 
-
 window.addEventListener("DOMContentLoaded", function () {
-  // When the document is loaded, attach event listeners
-
   // Attach event listener to show the form
   let addInvoiceButton = document.getElementById("add-invoice");
   addInvoiceButton.addEventListener("click", showInvoiceForm);
@@ -194,9 +211,3 @@ window.addEventListener("DOMContentLoaded", function () {
   // Populate clients dropdown
   populateClientsDropdown();
 });
-
-
-
-
-
-
